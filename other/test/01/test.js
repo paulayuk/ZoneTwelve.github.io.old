43d790718e61ba814c2a1e4773e14b7e735bb33d
ZoneTwelve <zonetwelve159@gmail.com>
2016-10-15 17:56:30 +0800

    Update test.js

--- other/test/01/test.js
+++ other/test/01/test.js
@@ -2,7 +2,7 @@ function encode(){
 	var text = document.getElementById("de-text").value;
 	var codeList = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 	for (var i=0;i<codeList.length;i++){
-		eval('text = text.replace(/'+codeList[i]+'/gi,"'+i+'")');
+		eval('text = text.replace(/'+codeList[i]+'/gi,"'+((i*100)+(i**5))+'")');
 	}
 	document.getElementById("en-text").value = text;
 }
